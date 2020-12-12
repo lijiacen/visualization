c8b4dbeb97f11e2a73743c578830b0b14bf9f37d
lijiacen <50915968+lijiacen@users.noreply.github.com>
2020-12-12 22:14:50 +0800

    v-charts实现百度地图

---
+++ vue2_echarts/src/components/MapView/components/map/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <ve-bmap height="100%" :settings="chartSettings" :series="chartSeries" :tooltip="chartTooltip"></ve-bmap>
+</template>
+
+<script>
+export default {
+  data() {
+    this.chartSettings = {
+      key: 'Y2u3xAZvnwzPjd3AMnobg9dDuwG4RoYN',
+      bmap: {
+        center: [120, 30],
+        zoom: 14,
+        roam: true,
+        mapStyle: {
+        }
+      }
+    };
+    this.chartTooltip = { show: true };
+    return {
+      chartSeries: [
+        {
+          type: 'scatter',
+          coordinateSystem: 'bmap',
+          data: [
+            [120, 30, 1] // 经度，维度，value，...
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
